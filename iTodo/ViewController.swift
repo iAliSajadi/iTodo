d69bedb23a28fc501cb15e642f88e248569fd1ce
ali Sajadi <chargoonmobileintern2@outlook.com>
2020-09-29 17:28:41 +0330

    Initial Commit

---
+++ iTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTodo
+//
+//  Created by Ali Sajadi on 9/29/20.
+//  Copyright © 2020 Ali Sajadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
