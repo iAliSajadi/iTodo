f443df77f335191db849644e1c997f4c8dc2c4e7
ali Sajadi <chargoonmobileintern2@outlook.com>
2020-09-29 19:39:37 +0330

    Implement and modify UI

---
+++ iTodo/ViewControllers/AddNewTodoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewTodoViewController.swift
+//  iTodo
+//
+//  Created by Ali Sajadi on 9/29/20.
+//  Copyright © 2020 Ali Sajadi. All rights reserved.
+//
+
+import UIKit
+
+class AddNewTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
